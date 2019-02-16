092cb9d028cc33815663ad2dad7e7257cde93d17
Nodnarb3 <brandon.slack@live.ca>
2019-02-16 11:20:18 -0700

    Basic Sensor data reading

--- app/src/main/java/com/cpsc581/sensorunlock/MainActivity.java
+++ app/src/main/java/com/cpsc581/sensorunlock/MainActivity.java
@@ -2,12 +2,56 @@ package com.cpsc581.sensorunlock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+/*
+* Referenced from https://www.javacodegeeks.com/2013/09/android-compass-code-example.html
+* */
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "SensorUnlock";
+    private SensorManager sensorManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // for the system's orientation sensor registered listeners
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR));
+    }
+
+    public void onSensorChanged(SensorEvent event)
+    {
+        //TODO Use this value to rotate something on the screen
+
+        Log.v(TAG, "Degrees:" + Float.toString(Math.round(event.values[0])));
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int value){
+        return;
     }
 }
