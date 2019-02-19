b0739fe47a4979edae4b6476f202f97983b61565
Shaheed M <shem.murji@gmail.com>
2019-02-18 17:19:29 -0700

    Added Colour, added new shape to modify

--- app/src/main/java/com/cpsc581/sensorunlock/MainActivity.java
+++ app/src/main/java/com/cpsc581/sensorunlock/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cpsc581.sensorunlock;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Sensor;
@@ -7,6 +8,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.hardware.SensorEventListener;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     ImageView targetImage;
     TextView degreeText;
+    View background;
     float currentDegree = 0f;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,8 +42,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         oSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
 
-        targetImage = findViewById(R.id.targetView);
+
+        targetImage = (ImageView) findViewById(R.id.targetView);
         degreeText = (TextView) findViewById(R.id.degreeTextView);
+        background = (View) findViewById(R.id.background);
     }
 
     @Override
@@ -58,17 +63,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     {
         super.onPause();
 
-        sensorManager.unregisterListener(this, oSensor);
+        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event)
     {
-        if(event.sensor.getType() == Sensor.TYPE_ORIENTATION){
+        if(event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
             getOrientation(event);
         }
-
     }
+
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int value){
         return;
@@ -79,7 +85,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         //timeView.setText(Float.toString(Math.round(event.values[0])));
         float degree = Math.round(event.values[0]);
-
 
         RotateAnimation rotateAnimation = new RotateAnimation(currentDegree,-degree, Animation.RELATIVE_TO_PARENT, 0f, Animation.RELATIVE_TO_PARENT, 0.25f);
 
@@ -94,6 +99,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             check = degree;
             Log.v(TAG, "Degrees:" + Float.toString(check));
             degreeText.setText(Float.toString(check) + "°");
+            if(check >= 0 && check < 90){
+                background.setBackgroundColor(Color.BLUE);
+                degreeText.setTextColor(Color.WHITE);
+            } else if(check >= 90 && check < 180){
+                background.setBackgroundColor(Color.GREEN);
+                degreeText.setTextColor(Color.WHITE);
+            } else if(check >= 180 && check < 270){
+                background.setBackgroundColor(Color.MAGENTA);
+                degreeText.setTextColor(Color.BLACK);
+            } else if(check >= 270 && check <= 360){
+                background.setBackgroundColor(Color.YELLOW);
+                degreeText.setTextColor(Color.BLACK);
+            }
         }
     }
 }
